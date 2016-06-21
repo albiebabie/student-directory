91c9be0bec675737e820b4857ca7065b0fd8be2f
albie <albiebabie@gmail.com>
2016-06-21 12:27:04 +0100

    using a hash instead

--- directory.rb
+++ directory.rb
@@ -1,46 +1,39 @@
 # let's put all the students into an array
 
 students = [
-  "Dr. Hannibal Lecter",  # makes it easier to add new names to the list,
-  "Darth Vader",          # rather than "puts name" each time.
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter",         cohort: :november},
+  {name:"Darth Vader",                  cohort: :november},
+  {name:"Nurse Ratched",                cohort: :november},
+  {name:"Michael Corleone",             cohort: :november},
+  {name:"Alex DeLarge",                 cohort: :november},
+  {name:"The Wicked Witch of the West", cohort: :november},
+  {name:"Terminator",                   cohort: :november},
+  {name:"Freddy Krueger",               cohort: :november},
+  {name:"The Joker",                    cohort: :november},
+  {name:"Joffrey Baratheon",            cohort: :november},
+  {name:"Norman Bates",                 cohort: :november}
 ]
-def print_header                            # extract the code into methods
-  puts "The students of Villains academy"   # First, print the header
-  puts "-------------"                      # Second, print the list of students
-end                                         # Final, print the header
-  # Note! we are now passing the students
-  # variable to the methods as an argument
-  # called "names". This is because, the
-  # methods don't have access to the local
-  # variables defined outside them.
 
-def print(names)
-names.each do |name|       # This code takes every name fromt the student array
-  puts name                # and for each name it executes the block of code
-  end                      # between the do and end. Each execution of this
-end                        # code is called an interation. On every iteration,
-                           # the current name will be assigned to the value
-                           # student which was specified between the | |
+# Extract the code into methods:
+# First, print the header
+# Second, print the list of students
+# Final, print the header
 
-# FIRST ITERATION
-#     1) takes the first value from the array (Dr. Hannibal)
-#     2) assigns it to the variable student
-#     3) then executes the block of code (puts student) which print it to the screen
+def print_header
+  puts "The students of Villains academy"
+  puts "-------------"
+end
 
-# SECOND AND MORE...
-#     Will continue to iterate through each value(name) in the array.
 
-def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+def print(students)
+students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+
+def print_footer(students)
+puts "Overall, we have #{students.count} great students"
 end
 # nothing happens until we call the methods >>
 print_header
