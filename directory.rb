0429a99316c76e55801ccccfb786c6321d101ba7
albie <albiebabie@gmail.com>
2016-06-21 11:45:51 +0100

    created methods

--- directory.rb
+++ directory.rb
@@ -1,9 +1,8 @@
 # let's put all the students into an array
-# makes it easier to add new names to the list,
-# rather than "puts name" each time.
+
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
+  "Dr. Hannibal Lecter",  # makes it easier to add new names to the list,
+  "Darth Vader",          # rather than "puts name" each time.
   "Nurse Ratched",
   "Michael Corleone",
   "Alex DeLarge",
@@ -14,14 +13,21 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-# and then print them out
-puts "The students of Villains academy"
-puts "-------------"
+def print_header                            # extract the code into methods
+  puts "The students of Villains academy"   # First, print the header
+  puts "-------------"                      # Second, print the list of students
+end                                         # Final, print the header
+  # Note! we are now passing the students
+  # variable to the methods as an argument
+  # called "names". This is because, the
+  # methods don't have access to the local
+  # variables defined outside them.
 
-students.each do |student| # This code takes every name fromt the student array
-  puts student             # and for each name it executes the block of code
-end                        # between the do and end. Each execution of this
-                           # code is called an interation. On every iteration,
+def print(names)
+names.each do |name|       # This code takes every name fromt the student array
+  puts name                # and for each name it executes the block of code
+  end                      # between the do and end. Each execution of this
+end                        # code is called an interation. On every iteration,
                            # the current name will be assigned to the value
                            # student which was specified between the | |
 
@@ -29,8 +35,14 @@ end                        # between the do and end. Each execution of this
 #     1) takes the first value from the array (Dr. Hannibal)
 #     2) assigns it to the variable student
 #     3) then executes the block of code (puts student) which print it to the screen
+
 # SECOND AND MORE...
 #     Will continue to iterate through each value(name) in the array.
 
-# finally. we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end
+# nothing happens until we call the methods >>
+print_header
+print(students)
+print_footer(students)
