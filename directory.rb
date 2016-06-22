a3bce7a639c2adbda4a182da7e90245a8427ce6d
albie <albiebabie@gmail.com>
2016-06-22 17:47:53 +0100

    8. Exercises 4) - Using While to Loop

--- directory.rb
+++ directory.rb
@@ -1,40 +1,33 @@
+def input_students
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
+  students = []
+  name = gets.chomp
+
+  until name.empty?
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 def print_header
- puts 'The students of my cohort at Makers Academy'
+  puts 'The students of my cohort at Makers Academy'
   puts '-------------'
 end
 
 def print(students)
-students.map do |student|
-  name = student[:name]
-  if name.length < 12
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  index = 0
+  while students.length > index
+    puts "#{students[index][:name]} (#{students[index][:cohort]} cohort)"
+    index += 1
   end
-end
 end
 
 def print_footer(names)
- puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students"
 end
-
-def input_students
- puts "Please enter the names of the students"
- puts "To finish, just hit return twice"
-
- students = []
-
- name = gets.chomp
-
- while !name.empty? do
-
-   students << {name: name, cohort: :november}
-   puts "Now we have #{students.count} students"
-
-   name = gets.chomp
- end
-
- students
-end
-
 students = input_students
 print_header
 print(students)
