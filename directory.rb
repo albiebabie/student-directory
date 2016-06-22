86c0e45f4c0a6e568d5cab7896ed6bf371f87485
albie <albiebabie@gmail.com>
2016-06-22 13:56:38 +0100

    8. Exercises 2) - Returning Names by Letters

--- directory.rb
+++ directory.rb
@@ -1,36 +1,41 @@
-def input_students                                    # method "input_students"
-  puts 'Please enter the names of the students'       # prints string to screen
-  puts 'To finish, just hit return twice'             # prints string to screen
-
-  students = [] # creates and empty array to store our future students
-  name = gets.chomp # gets the first name inputted by user
-
-  until name.empty? # until the name is empty, repeats this code
-    students << { name: name, cohort: :november } # add student hash to the array
-    puts "Now we have #{students.count} students" # print string inc interpolation
-
-    name = gets.chomp # get another name from the user
-  end
-  students # return the array of students
+def print_header
+ puts 'The students of my cohort at Makers Academy'
+  puts '-------------'
 end
 
-def print_header # method called "print_header"
-  puts 'The students of Villains academy' # prints string to screen
-  puts '-------------' # prints string to screen
-end
-
-def print(students) # method called "print" argument(students)
-  students.each_with_index do |student, tom| # iterate through each students and assign to block value "student"
-  puts "#{tom+1}. #{student[:name]} (#{student[:cohort]} cohort)" # printing the string with interpolations
+def print(students)
+students.map do |student|
+  name = student[:name][0].downcase
+  if name.downcase.start_with?('j')
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+end
 
-def print_footer(students) # method "called print_footer" argument(students)
-  puts "Overall, we have #{students.count} great students" # prints string with interpolation
+def print_footer(names)
+ puts "Overall, we have #{names.count} great students"
+end
+
+def input_students
+ puts "Please enter the names of the students"
+ puts "To finish, just hit return twice"
+ # create an empty array
+ students = []
+ # get the first name
+ name = gets.chomp
+
+ while !name.empty? do
+
+   students << {name: name, cohort: :november}
+   puts "Now we have #{students.count} students"
+
+   name = gets.chomp
+ end
+
+ students
 end
 
 students = input_students
-# nothing happens until we call the methods>>
 print_header
 print(students)
 print_footer(students)
