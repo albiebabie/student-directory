1d3540a9348666518a03f84ab57e348fd38f52df
albie <albiebabie@gmail.com>
2016-06-23 20:43:55 +0100

    8. Exercises 10) - different method to chomp

--- directory.rb
+++ directory.rb
@@ -1,56 +1,62 @@
 $months = ['January', 'February', 'March',
-          'April', 'May', 'June', 'July',
-          'August', 'September', 'October',
-          'November', 'December']
+           'April', 'May', 'June', 'July',
+           'August', 'September', 'October',
+           'November', 'December']
+
 def input_students
+
   students = []
   name = 'Alan'
   default = 'July'
+
   until name.empty?
     puts 'Please enter the name of a student'
-    name = gets.chomp.capitalize
+    name = gets.chop.capitalize
     break if name.empty?
     puts 'Please enter your cohort (using full month)'
-    cohort = gets.chomp.capitalize
+    cohort = gets.chop.capitalize
     puts 'Please enter your hobbies'
-    hobbies = gets.chomp.capitalize
+    hobbies = gets.chop.capitalize
     puts 'Please enter your country of birth'
-    country = gets.chomp.capitalize
+    country = gets.chop.capitalize
     puts 'Please enter your height'
-    height = gets.chomp
+    height = gets.chop
+
     if cohort.empty? or !$months.include?(cohort)
       cohort = default
     end
-
     students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height}
 
     if students.count == 1
-       puts "Now we have #{students.count} student."
-     else
-       puts "Now we have #{students.count} students."
+      puts "Now we have 1 student."
+    else
+      puts "Now we have #{students.count} students."
     end
-        puts 'To finish, just hit return twice. Or...'
+    puts 'To finish, just hit return twice. Or...'
   end
     students
 end
+
 def print_header
   puts 'The students of my cohort at Makers Academy:'
   puts '-------------------------------------------'
 end
+
 def print(students)
   index = 0
   while students.length > index
     puts "#{index + 1}. #{students[index][:name].center(10)} #{students[index][:cohort].to_s.center(15)} " +
-          "#{students[index][:hobbies].center(15)} #{students[index][:country].center(15)} " +
-          "#{students[index][:height].center(15)}"
+         "#{students[index][:hobbies].center(15)} #{students[index][:country].center(15)} " +
+         "#{students[index][:height].center(15)}"
     index += 1
   end
 end
+
 def print_by_cohort(students)
   cohort_month = []
   puts "To sort students by Cohort"
   puts "Enter a Month (no abbreviations)"
-  month = gets.chomp.capitalize
+  month = gets.chop.capitalize
   students.map do |student|
     if student[:cohort] == month
       cohort_month << [student[:name], student[:hobbies], student[:country], student[:height]]
@@ -61,6 +67,7 @@ def print_by_cohort(students)
   puts cohort.each { |details| details }.join(" ")
   end
 end
+
 def print_footer(names)
   if names.length == 1
     puts "Overall, we have 1 great student"
