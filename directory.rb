5bbf50d9d3e373b79f06b58c5bd6466ca7adf3bb
albie <albiebabie@gmail.com>
2016-06-23 16:14:05 +0100

    8. Exercises 9) - Adjusted to singular not just plural

--- directory.rb
+++ directory.rb
@@ -7,7 +7,7 @@ def input_students
   name = 'Alan'
   default = 'July'
   until name.empty?
-    puts 'Please enter the name of the student'
+    puts 'Please enter the name of a student'
     name = gets.chomp.capitalize
     break if name.empty?
     puts 'Please enter your cohort (using full month)'
@@ -21,14 +21,20 @@ def input_students
     if cohort.empty? or !$months.include?(cohort)
       cohort = default
     end
-    puts 'To finish, just hit return twice'
+
     students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height}
-    puts "Now we have #{students.count} students"
+
+    if students.count == 1
+       puts "Now we have #{students.count} student."
+     else
+       puts "Now we have #{students.count} students."
+    end
+        puts 'To finish, just hit return twice. Or...'
   end
     students
 end
 def print_header
-  puts 'The students of my cohort at Makers Academy'
+  puts 'The students of my cohort at Makers Academy:'
   puts '-------------------------------------------'
 end
 def print(students)
@@ -42,21 +48,25 @@ def print(students)
 end
 def print_by_cohort(students)
   cohort_month = []
-  puts "Sort by specific cohort - Please Enter Full Month"
+  puts "To sort students by Cohort"
+  puts "Enter a Month (no abbreviations)"
   month = gets.chomp.capitalize
   students.map do |student|
     if student[:cohort] == month
-      cohort_month << [student[:name], student[:cohort], student[:hobbies], student[:country], student[:height]]
+      cohort_month << [student[:name], student[:hobbies], student[:country], student[:height]]
     end
   end
-  puts "Cohort #{month}"
-  puts "Students are: "
+  puts "Students in #{month} Cohort:"
   cohort_month.each do |cohort|
   puts cohort.each { |details| details }.join(" ")
   end
 end
 def print_footer(names)
+  if names.length == 1
+    puts "Overall, we have 1 great student"
+  else
   puts "Overall, we have #{names.count} great students"
+end
 end
 students = input_students
 print_header
