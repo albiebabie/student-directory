ecf8188899d0522afe6a738e92c80c7e9855f6ee
albie <albiebabie@gmail.com>
2016-06-24 14:34:38 +0100

    10. Refactoring the code

--- directory.rb
+++ directory.rb
@@ -1,37 +1,8 @@
-$months = ['January', 'February', 'March',
-           'April', 'May', 'June', 'July',
-           'August', 'September', 'October',
-           'November', 'December']
-
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts '1. Input the Students'
-    puts '2. Show the Students'
-    puts '9. Exit'
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when '1'
-     students = input_students
-    when '2'
-     print_header
-     print_students(students)
-     print_footer(students)
-    when '9'
-     exit
-    else
-     puts 'I don\t know what you mean, please try again!'
-   # 4. repeat from step 1
-    end
-  end
-end
+@months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+           'August', 'September', 'October','November', 'December']
+@students = [] # empty array that can be accessed by all methods
 
 def input_students
-
-  students = []
   name = 'Alan'
   default = 'July'
 
@@ -47,38 +18,73 @@ def input_students
     country = gets.chomp.capitalize
     puts 'Please enter your height'
     height = gets.chomp
-    if cohort.empty? or !$months.include?(cohort)
+    if cohort.empty? or !@months.include?(cohort)
       cohort = default
     end
-    students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height}
-    if students.count == 1
+    @students << { name: name, cohort: cohort, hobbies: hobbies,
+                    country: country, height: height }
+    if @students.count == 1
       puts 'Now we have 1 student.'
     else
-      puts "Now we have #{students.count} students."
+      puts "Now we have #{@students.count} students."
     end
-    puts 'To finish, just hit return twice. Or...'
+    puts 'If you\'re finished entering students, just hit return twice. Or...'
   end
-    students
+    @students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts '1. Input the students'
+  puts '2. Show the students'
+  puts '9. Exit'
+end
+
+def show_students
+  print_header
+  print_student_details
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when '1'
+    input_students
+  when '2'
+    show_students
+  when '9'
+    exit
+  else
+    puts 'I don\'t know what you mean...'
+    puts 'Please try again!'
+  end
 end
 
 def print_header
-  puts 'The students of my cohort at Makers Academy:'
-  puts '-------------------------------------------'
+  puts 'The students of Villains Academy:'
+  puts '---------------------------------'
 end
 
-def print_students(students)
+def print_student_details
   index = 0
-  while students.length > index
-    puts "#{index + 1}. #{students[index][:name].center(10)} #{students[index][:cohort].to_s.center(15)} " +
-         "#{students[index][:hobbies].center(15)} #{students[index][:country].center(15)} " +
-         "#{students[index][:height].center(15)}"
+  @students.each do |student|
+
+    puts "#{index + 1}. #{@students[index][:name].center(10)} #{@students[index][:cohort].to_s.center(15)} " +
+         "#{@students[index][:hobbies].center(15)} #{@students[index][:country].center(15)} " +
+         "#{@students[index][:height].center(15)}"
     index += 1
   end
 end
 
-def print_by_cohort(students)
+def print_by_cohort
   cohort_month = []
-  if students.empty?
+  if @students.empty?
     exit
   else
     puts 'To sort students by Cohort'
@@ -92,17 +98,18 @@ def print_by_cohort(students)
   end
   puts "Students in #{month} Cohort:"
   cohort_month.each do |cohort|
-  puts cohort.each { |details| details }.join(" ")
+  puts cohort.join(' ')
   end
 end
 
-def print_footer(names)
-  if names.empty?
+def print_footer
+
+  if @students.empty?
     puts 'There are no students in this cohort!'
-  elsif names.length == 1
+  elsif @students.length == 1
     puts 'Overall, we have 1 great student'
   else
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
