cf42df01a79bd389bbd68f2cd57139fac1f3ff3a
albie <albiebabie@gmail.com>
2016-06-24 15:47:28 +0100

    11. Saving the data to a file

--- directory.rb
+++ directory.rb
@@ -2,6 +2,18 @@
            'August', 'September', 'October','November', 'December']
 @students = [] # empty array that can be accessed by all methods
 
+def save_students
+  # open the file for writing
+  file = File.open('students.csv', 'w')
+  # iterate over the arry of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:hobbies], student[:country], student[:height]]
+    csv_line = student_data.join(',')
+    file.puts csv_line
+  end
+  file.close
+end
+
 def input_students
   name = 'Alan'
   default = 'July'
@@ -43,6 +55,7 @@ end
 def print_menu
   puts '1. Input the students'
   puts '2. Show the students'
+  puts '3. Save the list to students.csv'
   puts '9. Exit'
 end
 
@@ -58,6 +71,8 @@ def process(selection)
     input_students
   when '2'
     show_students
+  when '3'
+    save_students
   when '9'
     exit
   else
