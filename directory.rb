d59dd76958ccd8094887ed6716060b8cab11d2c9
albie <albiebabie@gmail.com>
2016-06-27 18:44:17 +0100

    14.6 Open File Code Block

--- directory.rb
+++ directory.rb
@@ -72,22 +72,22 @@ end
 
 def save_students
   puts 'Please enter a file name:'
-  file = File.open(gets.chomp, 'w')
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(',')
-    file.puts csv_line
+  File.open(gets.chomp, 'w') do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(',')
+      file.puts csv_line
+    end
   end
-  file.close
 end
 
 def load_students(filename = 'students.csv')
-  file = File.open(filename, 'r')
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-  add_students(name, cohort)
+  File.open(filename, 'r') do |file|
+    file.readlines.each do |line|
+      name, cohort = line.chomp.split(',')
+      add_students(name, cohort)
+    end
   end
-  file.close
 end
 
 def try_load_students
