f12deac61fd8b30fbf50a9e327c3e001dc81b987
albie <albiebabie@gmail.com>
2016-06-28 16:13:32 +0100

    14.5 Load and Save User Input Ammended

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-require 'csv'
 @students = []
 
 def print_menu
@@ -73,20 +72,22 @@ end
 
 def save_students
   puts 'Please enter a file name:'
-  CSV.open(gets.chomp, 'w') do |file|
-    @students.each do |student|
-      file << [student[:name], student[:cohort]]
-    end
+  file = File.open(gets.chomp, 'w')
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(',')
+    file.puts csv_line
   end
+  file.close
 end
 
-def load_students(filename = 'students.csv')
-  File.open(filename, 'r') do |file|
-    file.readlines.each do |line|
-      name, cohort = line.chomp.split(',')
-      add_students(name, cohort)
-    end
+def load_students(filename)
+  file = File.open(filename, 'r')
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+  add_students(name, cohort)
   end
+  file.close
 end
 
 def try_load_students
